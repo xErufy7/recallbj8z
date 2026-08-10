2a68b4836fc274e7d2b286c921b7a14930e8b5e6
xErufy7 <553280497@qq.com>
2026-08-10 17:25:51 +0800

    更新更新日志

--- data/constants.ts
+++ data/constants.ts
@@ -2,7 +2,7 @@
 import { Difficulty, GeneralStats } from '../types';
 
 export const CHANGELOG_DATA = [
-    { version: 'v1.5/merge', date: '2026-8-8', content: ['融合新版视觉与旧版特性', '新增排行榜系统', '新增自定义API配置', '天赋被动系统', '每难度独立存档'] },
+    { version: 'v1.5/merge', date: '2026-8-8', content: ['融合新版视觉与旧版特性', '新增排行榜系统', '新增自定义API配置', '天赋被动系统', '每难度独立存档', '优化弹窗阴影遮盖效果'] },
     { version: 'v1.4', date: '2026-5-21', content: ['优化了主界面排版', '新增自定义 API 配置', '保存进度方式改为自动保存', '优化了排行榜的显示', '新增重置成就功能', '新增代理节点默认 API Key'] },
     { version: 'v1.3/beta', date: '2026-1-22', content: ['新增【无限重开 AI版】，接入 Gemini API 实现动态事件。', '优化了负债逻辑，现在负债会有分级 Debuff。'] },
     { version: 'v1.2/testing', date: '2026-1-21', content: ['实装了每日挑战','修复了已知问题'] },
